82146d8059d35cf53a5acb5ea8b83462b98604eb
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-08-06 20:23:46 +0000

    added some comments for clarity, and some console.logs for the user to confirm each step is being completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,11 @@
 var request = require('request');
 var secrets = require('./secrets.js')
 var fs = require('fs');
+// top set of variables call the modules that are required
+
 
 console.log("Welcome to the Github Avatar Downloader!")
-
+// intro to confirm the program is running
 
 
 
@@ -11,13 +13,16 @@ console.log("Welcome to the Github Avatar Downloader!")
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    // using variables for repoOwner and repoName allow for modular code
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
+      'Authorization': secrets.GITHUB_TOKEN // GITHUB_TOKEN format in secrets.js is "token token_number"
     }
   };
   request(options, function(err, res, body) {
     cb(JSON.parse(err), JSON.parse(body));
+    //parsing the JSON here presents the err and body as an object for the callback function
+    //callback function will download the image with a given url
   });
 }
 
@@ -30,8 +35,13 @@ var downloadImageByURL = function(url, filePath) {
          })
          .on("response", function(chunk) {
           console.log("Saving images now!")
+          //confirms that file save is starting
          })
-         .pipe(fs.createWriteStream(filePath))
+         .pipe(fs.createWriteStream(filePath).on('finish', function() {
+          console.log("Your images are saved!")
+          //after the emitter stream (write) finishes for each file, a message will be displayed to confirm the save
+         }))
+
 }
 
 
@@ -39,13 +49,16 @@ var downloadImageByURL = function(url, filePath) {
 
 
 getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
+//process.argv[2] and process.argv[3] are the inputted repoOwner and repoName
   if (!process.argv[2] || !process.argv[3]) {
     console.log("give me some values bruh")
+    //error result if either field is left blank
   }
   else {
     console.log("Errors:", err);
     for (var contributor of result) {
       var filePath = `./avatars/${contributor.login}.jpg`
+      //file path is generated with login info from the requested JSON
       downloadImageByURL(contributor.avatar_url, filePath)
     }
   }
