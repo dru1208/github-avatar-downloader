3f8172a731a7c31dd35c6ac5e474fc68398ae4b1
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-08-06 18:24:04 +0000

    get avatars made

---
+++ download_avatars.js
@@ -0,0 +1,28 @@
+var request = require('request');
+var secrets = require('./secrets.js')
+
+console.log("Welcome to the Github Avatar Downloader!")
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    },
+    Authorization: secrets
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+
+
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
